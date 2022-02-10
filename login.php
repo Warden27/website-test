62327930cfc59274d1f9940c46f65efcf56160ef
Bernard Kruger <bernardk@intelliacc.com>
2022-02-10 22:19:58 +0200

    reg/log

--- login.php
+++ login.php
@@ -76,6 +76,18 @@
     .pad {
       padding:30px;
     }
+
+    .btnColor {
+      color: white;
+  background: #305485;
+  border:0px;
+    }
+    .test {
+    /* background-color:yellowgreen; */
+    /* width: 80%; */
+    margin-right:auto;
+    margin-left:auto;
+}
   </style>
 </head>
 
@@ -95,7 +107,7 @@
   </div>
   
 
-  <section class="vh-100">
+  <section class="vh-50 w-50 border border-light test">
     <div class="container-fluid h-custom pad">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <!-- <div class="col-md-9 col-lg-6 col-xl-5">
@@ -104,59 +116,49 @@
         </div> -->
         <!-- <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1"> -->
           <div>
-            <form>
-            <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-              <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-              <button type="button" class="btn btn-primary btn-floating mx-1">
-                <i class="fa fa-facebook-f"></i>
-              </button>
-  
-              <button type="button" class="btn btn-primary btn-floating mx-1">
-                <i class="fa fa-twitter"></i>
-              </button>
-  
-              <button type="button" class="btn btn-primary btn-floating mx-1">
-                <i class="fa fa-linkedin"></i>
-              </button>
-            </div>
-  
-            <div class="divider d-flex align-items-center my-4">
-              <p class="text-center fw-bold mx-3 mb-0">Or</p>
-            </div>
-  
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-              <input type="email" id="form3Example3" class="form-control form-control-lg"
-                placeholder="Enter a valid email address" />
-              <label class="form-label" for="form3Example3">Email address</label>
-            </div>
-  
-            <!-- Password input -->
-            <div class="form-outline mb-3">
-              <input type="password" id="form3Example4" class="form-control form-control-lg"
-                placeholder="Enter password" />
-              <label class="form-label" for="form3Example4">Password</label>
-            </div>
-  
-            <div class="d-flex justify-content-between align-items-center">
-              <!-- Checkbox -->
-              <div class="form-check mb-0">
-                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                <label class="form-check-label" for="form2Example3">
-                  Remember me
-                </label>
-              </div>
-              <a href="#!" class="text-body">Forgot password?</a>
-            </div>
-  
-            <div class="text-center text-lg-start mt-4 pt-2">
-              <button type="button" class="btn btn-primary btn-lg"
-                style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-              <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href=""
-                  class="link-danger">Register</a></p>
-            </div>
-  
-          </form>
+            <!-- Default form login -->
+<form class="text-center border border-light p-5" action="#!">
+
+<p class="h4 mb-4">Login</p>
+
+<!-- Email -->
+<input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+<!-- Password -->
+<input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
+
+<div class="d-flex justify-content-around">
+    <div>
+        <!-- Remember me -->
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+            <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+        </div>
+    </div>
+    <div>
+        <!-- Forgot password -->
+        <a href="">Forgot password?</a>
+    </div>
+</div>
+
+<!-- Login button -->
+<button class="btn btn-info btn-block my-4 btnColor" type="submit">Login</button>
+
+<!-- Register -->
+<p>Not a member?
+    <a href="register.php">Register</a>
+</p>
+
+<!-- Social login -->
+<p>or Login with:</p>
+
+<a href="#" class="mx-3 fa-lg" role="button"><i class="fa fa-facebook-f light-blue-text"></i></a>
+<a href="#" class="mx-3 fa-lg" role="button"><i class="fa fa-twitter light-blue-text"></i></a>
+<a href="#" class="mx-3 fa-lg" role="button"><i class="fa fa-linkedin light-blue-text"></i></a>
+<a href="#" class="mx-3 fa-lg" role="button"><i class="fa fa-github light-blue-text"></i></a>
+
+</form>
+<!-- Default form login -->
         </div>
       </div>
     </div>
--- register.php
+++ register.php
@@ -1,147 +1,204 @@
-<?php
-// Include config file
-require_once "config.php";
- 
-// Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
-        $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-    
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-            
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
-                header("location: login.php");
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
-    // Close connection
-    mysqli_close($link);
-}
-?>
- 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    </style>
+  <title>Login</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.0.2 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <style>
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .navbar {
+      width: 100%;
+      background-color: #555;
+      overflow: auto;
+    }
+
+    .navbar a {
+      float: left;
+      padding: 12px;
+      color: white;
+      text-decoration: none;
+      font-size: 17px;
+    }
+
+    .navbar a:hover {
+      /* background-color: #000; */
+    }
+
+    .active {
+      background-color: #04aa6d;
+    }
+
+    @media screen and (max-width: 500px) {
+      .navbar a {
+        float: none;
+        display: block;
+      }
+    }
+  </style>
+  <style>
+   * {
+      box-sizing: border-box;
+    }
+
+    /* Add a gray background color with some padding */
+    body {
+      font-family: Arial;
+      /* padding: 20px; */
+      background: #f1f1f1;
+    }
+
+  /* Header/Blog Title */
+    .header {
+      padding-bottom: 200px;
+      padding-top: 200px;
+      font-size: 40px;
+      text-align: center;
+      width: 100%;
+      background: white;
+      /* background-image: url(images/background.jpg); */
+      background-image: url(images/background2.jpg);
+      /* background-image: url(images/background3.jpg); */
+      background-size: cover;
+      color: #f1f1f1;
+    }
+
+    .pad {
+      padding:30px;
+    }
+
+    .btnColor {
+      color: white;
+  background: #305485;
+  border:0px;
+    }
+    .test {
+    /* background-color:yellowgreen; */
+    /* width: 80%; */
+    margin-right:auto;
+    margin-left:auto;
+}
+  </style>
 </head>
+
 <body>
-     <!-- Bootstrap JavaScript Libraries -->
+
+  <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
     integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
     crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
     integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
     crossorigin="anonymous"></script>
-    
-<?php include 'blocks/nav.php';?>
 
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
-            </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
-        </form>
-    </div>    
+  <?php include 'blocks/nav.php';?>
+  <div class="header">
+    <h2>The Blog Spot</h2>
+  </div>
+  
+
+  <section class="vh-50 w-50 border border-light test">
+    <div class="container-fluid h-custom pad">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <!-- <div class="col-md-9 col-lg-6 col-xl-5">
+          <img src="images/login.jpg" class="img-fluid"
+            alt="Sample image">
+        </div> -->
+        <!-- <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1"> -->
+          <div>
+            <!-- Default form login -->
+<!-- Default form register -->
+<form class="text-center border border-light p-5" action="#!">
+
+    <p class="h4 mb-4">Sign up</p>
+
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name">
+        </div>
+    </div>
+
+    <!-- E-mail -->
+    <input type="email" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+    <!-- Password -->
+    <input type="password" id="defaultRegisterFormPassword" class="form-control" placeholder="Password" aria-describedby="defaultRegisterFormPasswordHelpBlock">
+    <small id="defaultRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+        At least 8 characters and 1 digit
+    </small>
+
+    <!-- Phone number -->
+    <input type="text" id="defaultRegisterPhonePassword" class="form-control" placeholder="Phone number" aria-describedby="defaultRegisterFormPhoneHelpBlock">
+    <small id="defaultRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">
+        Optional - for two step authentication
+    </small>
+
+    <!-- Newsletter -->
+    <div class="custom-control custom-checkbox">
+        <input type="checkbox" class="custom-control-input" id="defaultRegisterFormNewsletter">
+        <label class="custom-control-label" for="defaultRegisterFormNewsletter">Subscribe to our newsletter</label>
+    </div>
+
+    <!-- Sign up button -->
+    <button class="btn btn-info my-4 btn-block btnColor" type="submit">Sign up</button>
+
+    <!-- Social register -->
+    <p>or sign up with:</p>
+
+    <a href="#" class="mx-3 fa-lg" role="button"><i class="fa fa-facebook-f light-blue-text"></i></a>
+    <a href="#" class="mx-3 fa-lg" role="button"><i class="fa fa-twitter light-blue-text"></i></a>
+    <a href="#" class="mx-3 fa-lg" role="button"><i class="fa fa-linkedin light-blue-text"></i></a>
+    <a href="#" class="mx-3 fa-lg" role="button"><i class="fa fa-github light-blue-text"></i></a>
+
+    <hr>
+
+    <!-- Terms of service -->
+    <p>By clicking
+        <em>Sign up</em> you agree to our
+        <a href="" target="_blank">terms of service</a>
+
+</form>
+<!-- Default form register -->
+<!-- Default form login -->
+        </div>
+      </div>
+    </div>
+    <!-- <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+      <div class="text-white mb-3 mb-md-0">
+        Copyright © 2020. All rights reserved.
+      </div>
+  
+      <div>
+        <a href="#!" class="text-white me-4">
+          <i class="fa fa-facebook-f"></i>
+        </a>
+        <a href="#!" class="text-white me-4">
+          <i class="fa fa-twitter"></i>
+        </a>
+        <a href="#!" class="text-white me-4">
+          <i class="fa fa-google"></i>
+        </a>
+        <a href="#!" class="text-white">
+          <i class="fa fa-linkedin"></i>
+        </a>
+      </div>
+    </div> -->
+  </section>
 </body>
+
 </html>
